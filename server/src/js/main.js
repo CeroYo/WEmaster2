0afbfcf564feb5db1b0298cecb8588eb5e05c3c0
CeroYo <40327451+CeroYo@users.noreply.github.com>
2018-06-17 16:15:20 +0200

    u4

--- server/src/js/main.js
+++ server/src/js/main.js
@@ -2,26 +2,17 @@ const express = require("express");
 const path = require("path");
 let server = express();
 
-const fs = require("fs");
+class Server {
+	constructor() {
+		let PORT = 8080;
 
-let htmlData = "";
-let cssData = "";
-let jsData = "";
+		if (!isNaN(process.argv[3])) {
+			PORT = process.argv[3];
+		}
 
-class Server{
-    constructor(){
+		server.use(express.static(path.join(process.argv[2])));
 
-        let PORT = 8080;
-
-        //Falls Parameter nach "npm run start" eine Nummer ist wird der Port überschrieben
-        if (!isNaN(process.argv[3])){
-            PORT = process.argv[3];
-        }
-                
-        server.use(express.static(path.join(process.argv[2])));
-        
-
-        server.listen(PORT, () => {console.log("HTTP Server listening on port %d.", PORT)});       
-    }
-    
-}let serverx = new Server();
+		server.listen(PORT, () => { console.log("HTTP Server listening on port %d.", PORT); });
+	}
+}
+let serverx = new Server();
